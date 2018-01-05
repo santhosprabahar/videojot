5618b5953d3f1032235fae69a7f0046c98d2352f
santhosprabahar <santhosprabahar@gmail.com>
2018-01-05 19:38:23 +0530

    Port Update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const path = require('path');
-const port = process.env.port || 4200;
+const port = process.env.PORT || 4200;
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const flash = require('connect-flash');
